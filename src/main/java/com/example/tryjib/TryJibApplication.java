e703e2dfbee06373aef910891ff617621b0ba78e
minami.yoshihiko <nannany@hey.jp>
2021-12-30 17:49:46 +0900

    image pushはまだ

---
+++ src/main/java/com/example/tryjib/TryJibApplication.java
@@ -0,0 +1,13 @@
+package com.example.tryjib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryJibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryJibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tryjib/TryJibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tryjib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryJibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
